743c518f87d6df8631bf905ba3e7cdfaecd7eba1
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-02-07 19:56:25 -0300

    Se agrega metodo que devuelve listado de personajes

--- src/main/java/com/api/disney/controladores/PersonajeController.java
+++ src/main/java/com/api/disney/controladores/PersonajeController.java
@@ -3,10 +3,7 @@ package com.api.disney.controladores;
 import com.api.disney.entidades.Personaje;
 import com.api.disney.servicios.PersonajeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -16,6 +13,11 @@ public class PersonajeController {
 
     @Autowired
     private PersonajeService personajeService;
+
+    @GetMapping
+    public String listadoPersojees(){
+        return personajeService.listadoPersonajes();
+    }
 
     @PostMapping
     public Personaje guardar(@RequestBody Personaje personaje, @RequestParam("file") MultipartFile image ) throws IOException {
--- src/main/java/com/api/disney/servicios/PersonajeService.java
+++ src/main/java/com/api/disney/servicios/PersonajeService.java
@@ -2,11 +2,15 @@ package com.api.disney.servicios;
 
 import com.api.disney.entidades.Personaje;
 import com.api.disney.repositorios.PersonajeRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PersonajeService {
@@ -20,6 +24,22 @@ public class PersonajeService {
     public Personaje guardar(Personaje personaje, MultipartFile image) throws IOException {
         guardadoArchivo.guardar(image);
         return personajeRepository.save(personaje);
+    }
+
+    public String listadoPersonajes(){
+        List<Personaje> listado = (ArrayList<Personaje>) personajeRepository.findAll();
+
+        JSONArray dataPrincipal = new JSONArray();
+
+        for (Personaje elemento: listado) {
+            JSONObject objUnitario = new JSONObject();
+            objUnitario.put("name", elemento.getNombre());
+            objUnitario.put("imagen", elemento.getImagen());
+            // Objeto dentro de objeto
+            dataPrincipal.put(objUnitario);
+        }
+        
+        return dataPrincipal.toString();
     }
 
 
