5a8735db92b696339fa350f75c028dbf6624b82f
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-02-07 18:27:57 -0300

    Se agerga metodo de creacion, se indica que se trata de un servicio

--- src/main/java/com/api/disney/controladores/PersonajeController.java
+++ src/main/java/com/api/disney/controladores/PersonajeController.java
@@ -1,4 +1,23 @@
 package com.api.disney.controladores;
 
+import com.api.disney.entidades.Personaje;
+import com.api.disney.servicios.PersonajeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController("/characters")
 public class PersonajeController {
+
+    @Autowired
+    private PersonajeService personajeService;
+
+    @PostMapping
+    public Personaje guardar(@RequestBody Personaje personaje, @RequestParam("file") MultipartFile image ){
+        return personajeService.guardar(personaje, image);
+    }
+
 }
