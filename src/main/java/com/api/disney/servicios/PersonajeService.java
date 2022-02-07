b532f14334f3ef7f37bb5e4de9e0d169e90b0f28
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-02-07 18:28:44 -0300

    Se indica que se trata de un servicio, se agrega metodo de guardado

--- src/main/java/com/api/disney/servicios/PersonajeService.java
+++ src/main/java/com/api/disney/servicios/PersonajeService.java
@@ -1,4 +1,20 @@
 package com.api.disney.servicios;
 
+import com.api.disney.entidades.Personaje;
+import com.api.disney.repositorios.PersonajeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
 public class PersonajeService {
+
+    @Autowired
+    private PersonajeRepository personajeRepository;
+
+    public Personaje guardar(Personaje personaje, MultipartFile image){
+        return personajeRepository.save(personaje);
+    }
+
+
 }
