ae29f301dec841674f291cf982b6cd280b43b4d2
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-02-15 17:32:22 -0300

    Se eliminan importaciones sin uso

--- src/main/java/com/api/disney/controladores/PersonajeController.java
+++ src/main/java/com/api/disney/controladores/PersonajeController.java
@@ -1,11 +1,8 @@
 package com.api.disney.controladores;
 
 import com.api.disney.entidades.Personaje;
-import com.api.disney.servicios.ArchivoService;
 import com.api.disney.servicios.PersonajeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
