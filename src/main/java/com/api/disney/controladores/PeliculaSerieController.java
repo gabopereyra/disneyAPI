7e9fe4a4b6291d5c50eb3e3f43ea824882919091
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-02-16 16:31:08 -0300

    Se agregan métodos básicos de CRUD para peliculas.

--- src/main/java/com/api/disney/controladores/PeliculaSerieController.java
+++ src/main/java/com/api/disney/controladores/PeliculaSerieController.java
@@ -1,4 +1,45 @@
 package com.api.disney.controladores;
 
+import com.api.disney.entidades.PeliculaSerie;
+import com.api.disney.entidades.Personaje;
+import com.api.disney.servicios.PeliculaSerieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
 public class PeliculaSerieController {
+    @Autowired
+    private PeliculaSerieService peliculaSerieService;
+
+    @GetMapping
+    public String listadoPeliculas(){
+        return peliculaSerieService.listadoPeliculas();
+    }
+
+    @PostMapping
+    public PeliculaSerie guardar(@ModelAttribute PeliculaSerie peliculaSerie, @RequestParam("file") MultipartFile image ) {
+        return peliculaSerieService.guardar(peliculaSerie, image);
+    }
+
+    @PutMapping("/{id}")
+    public PeliculaSerie modificar(@ModelAttribute PeliculaSerie peliculaSerie, @RequestParam("file") MultipartFile image, @PathVariable("id") Integer id) {
+        return peliculaSerieService.modificar(peliculaSerie, image, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void borrar(@PathVariable("id") Integer id){
+        peliculaSerieService.borrar(id);
+    }
+
+    @GetMapping(params = "id")
+    public PeliculaSerie obtenerPorId(@RequestParam("id") Integer id){
+        return peliculaSerieService.obtenerPorId(id);
+    }
+
+
 }
--- src/main/java/com/api/disney/servicios/PeliculaSerieService.java
+++ src/main/java/com/api/disney/servicios/PeliculaSerieService.java
@@ -1,4 +1,69 @@
 package com.api.disney.servicios;
 
+import com.api.disney.entidades.PeliculaSerie;
+import com.api.disney.entidades.Personaje;
+import com.api.disney.repositorios.PeliculaSerieRepository;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class PeliculaSerieService {
+    @Autowired
+    private PeliculaSerieRepository peliculaSerieRepository;
+
+    @Autowired
+    private ArchivoService fileService;
+
+    @Transactional
+    public PeliculaSerie guardar(PeliculaSerie peliculaSerie, MultipartFile image) {
+        fileService.uploadFile(image);
+        peliculaSerie.setImagen(image.getOriginalFilename());
+        peliculaSerie.setAlta(true);
+        return peliculaSerieRepository.save(peliculaSerie);
+    }
+
+    @Transactional
+    public PeliculaSerie modificar(PeliculaSerie peliculaSerie, MultipartFile image, Integer id) {
+        if(peliculaSerieRepository.findById(id).isPresent()){
+            fileService.uploadFile(image);
+            peliculaSerie.setImagen(image.getOriginalFilename());
+            return peliculaSerieRepository.save(peliculaSerie);
+        }
+        return null;
+    }
+
+    @Transactional
+    public void borrar(Integer id){
+        peliculaSerieRepository.deleteById(id);
+    }
+
+    @Transactional
+    public String listadoPeliculas(){
+        List<PeliculaSerie> listado = (ArrayList<PeliculaSerie>) peliculaSerieRepository.findAll();
+
+        JSONArray dataPrincipal = new JSONArray();
+
+        for (PeliculaSerie elemento: listado) {
+            JSONObject objUnitario = new JSONObject();
+            objUnitario.put("titulo", elemento.getTitulo());
+            objUnitario.put("imagen", elemento.getImagen());
+            objUnitario.put("fechaCreacion", elemento.getFechaCreacion());
+            // Objeto dentro de objeto
+            dataPrincipal.put(objUnitario);
+        }
+
+        return dataPrincipal.toString();
+    }
+
+    public PeliculaSerie obtenerPorId(Integer id){
+        return peliculaSerieRepository.getById(id);
+    }
+
 }
