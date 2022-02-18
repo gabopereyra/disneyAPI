5acb523dfd028928109ce5c46b019c43d793ebce
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-02-17 21:00:59 -0300

    Se adicionan metodos basicos de CRUD para Genero

--- src/main/java/com/api/disney/controladores/GeneroController.java
+++ src/main/java/com/api/disney/controladores/GeneroController.java
@@ -1,4 +1,37 @@
 package com.api.disney.controladores;
 
+import com.api.disney.entidades.Genero;
+import com.api.disney.servicios.GeneroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/genre")
 public class GeneroController {
+    @Autowired
+    private GeneroService generoService;
+
+    @GetMapping
+    public List<Genero> listadoGeneros(){
+        return generoService.listadoGenero();
+    }
+
+    @PostMapping
+    public Genero guardar(@ModelAttribute Genero genero, @RequestParam("file") MultipartFile image ) {
+        return generoService.guardar(genero, image);
+    }
+
+    @PutMapping("/{id}")
+    public Genero modificar(@ModelAttribute Genero genero, @RequestParam("file") MultipartFile image, @PathVariable("id") Integer id) {
+        return generoService.modificar(genero, image, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void borrar(@PathVariable("id") Integer id){
+        generoService.borrar(id);
+    }
+
 }
--- src/main/java/com/api/disney/servicios/GeneroService.java
+++ src/main/java/com/api/disney/servicios/GeneroService.java
@@ -1,4 +1,43 @@
 package com.api.disney.servicios;
 
+import com.api.disney.entidades.Genero;
+import com.api.disney.repositorios.GeneroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
 public class GeneroService {
+    @Autowired
+    private GeneroRepository generoRepository;
+
+    @Autowired
+    private ArchivoService fileService;
+
+    public List<Genero> listadoGenero() {
+        return generoRepository.findAll();
+    }
+
+    public Genero guardar(Genero genero, MultipartFile image) {
+        fileService.uploadFile(image);
+        genero.setImagen(image.getOriginalFilename());
+        genero.setAlta(true);
+        return generoRepository.save(genero);
+
+    }
+
+    public Genero modificar(Genero genero, MultipartFile image, Integer id) {
+        if(generoRepository.findById(id).isPresent()){
+            fileService.uploadFile(image);
+            genero.setImagen(image.getOriginalFilename());
+            return generoRepository.save(genero);
+        }
+        return null;
+    }
+
+    public void borrar(Integer id) {
+        generoRepository.deleteById(id);
+    }
 }
