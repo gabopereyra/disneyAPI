0fd43153fc6c5ebd38a78acfd5c9654a1b255890
Gabriel Pereyra <gabrielp1689@gmail.com>
2022-02-19 18:49:08 -0300

    Se modifica clase para adaptarse a JWT

--- src/main/java/com/api/disney/seguridad/SecurityConfig.java
+++ src/main/java/com/api/disney/seguridad/SecurityConfig.java
@@ -1,51 +1,22 @@
 package com.api.disney.seguridad;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Autowired
-//    private UsuarioServicio usuarioServicio;
-
-    @Autowired
-    private BCryptPasswordEncoder encoder;
-    
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-//        auth.userDetailsService(usuarioServicio).passwordEncoder(encoder);
-//    }
-    
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .addFilterAfter(new LoginFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests()
-                    .antMatchers("/auth/*").permitAll()
-                    .antMatchers("/**").permitAll()
-                .and()
-                .formLogin()
-                    .loginPage("/login")
-                        .loginProcessingUrl("/logincheck")
-                        .usernameParameter("username")
-                        .passwordParameter("password")
-                        .defaultSuccessUrl("/", true)
-                        .failureUrl("/login?error=true")
-                    .permitAll()
-                .and()
-                    .logout()
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout=true")
-                        .permitAll()
-                        .deleteCookies("JSESSIONID")
-                .and()
-                    .csrf()
-                    .disable();
+                .antMatchers(HttpMethod.POST, "/auth/login", "/auth/register").permitAll()
+                .anyRequest().authenticated();
     }
 }
